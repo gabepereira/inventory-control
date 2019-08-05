d95220dd85441760588b6a1dc2495b07e3babd2d
Gabriel Pereira <gabriel6562014@gmail.com>
2019-08-05 01:01:18 -0300

    Created basic login screen

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Login extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>Fala galera!</Text>
-            </View>  
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  onSubmit = () => {
+    this.props.navigation.navigate('Home');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.signIn}>
+          <TextInput style={styles.input}
+            placeholder='E-mail'
+            editable={true}
+            textContentType='emailAddress'
+            onChangeText={email => this.setState({email})}/>
+          <TextInput style={styles.input}
+            placeholder='Password'
+            editable={true}
+            textContentType='password'
+            secureTextEntry={true}
+            onChangeText={password => this.setState({password})}/>
+        </View>
+        <TouchableOpacity onPress={this.onSubmit}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </View>
+        </TouchableOpacity>
+      </View>  
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -21,8 +50,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffffff',
   },
+  signIn: {
+    margin: 10
+  },
   text: {
     textAlign: 'center',
     margin: 10
+  },
+  input: {
+    width: 200,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc'
+  },
+  button: {
+    margin: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    width: 'auto',
+    height: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#303030',
+    borderRadius: 3,
+  },
+  buttonText: {
+    color: '#ffffff'
   }
 });
